6b608fd73c85bd465eac4190af89bbc214685b80
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-04-08 10:51:25 -0300

    Add files via upload

---
+++ aula3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <span style="font-weight: bold;">Amanda</span>
+    <a href="pagina.php?tipo=C&nome=Amanda&sobrenome=Werner&idade=16">Clique Aqui</a>
+    <br>
+    <span style="font-weight: bold;">Sara</span>
+    <a href="pagina.php?tipo=C&nome=Sara&sobrenome=Luz&idade=17">Clique Aqui</a>
+</body>
+</html>
