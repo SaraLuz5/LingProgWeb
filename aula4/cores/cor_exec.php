2ca0803ac36a2c8bae32198d3dc77a49d3185d41
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-04-08 10:43:33 -0300

    Add files via upload

---
+++ aula4/cores/cor_exec.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$cor = $_POST["cor"];
+echo"<body style='background-color: " . $cor . ";'></body>";
---
+++ aula4/cores/cor_form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Escolha a cor</h1>
+    <form action="cor_exec.php" method="POST">
+
+    <select name="cor">
+        <option value="">---Selecione a cor---</option>
+        <option value="Tomato">Tomato</option>
+        <option value="Orange">Orange</option>
+        <option value="blue">Blue</option>
+        <option value="pink">Pink</option>
+        <option value="Gray">Gray</option>
+        <option value="black">Black</option>
+        <option value="Violet">Violet</option>
+        <option value="LigthGray">LigthGray</option>
+    </select>
+
+    <br><br>
+
+    <button>Enviar</button>
+
+    </form>
+</body>
+</html>
