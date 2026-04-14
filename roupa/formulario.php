4f5f3f6e9d528d200b645336479abe478de957e2
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-04-14 11:19:58 -0300

    Update formulario.php

--- roupa/formulario.php
+++ roupa/formulario.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap" rel="stylesheet">
+
     <title>Dados do Card</title>
 
 
@@ -18,6 +22,9 @@
             justify-content: center;
             display: flex;
             color: #613749;
+            font-family: "Imperial Script", cursive;
+            font-size: 80px;
+            
         }
 
         .geral {
