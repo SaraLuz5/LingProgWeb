e0caec3bd4b426af8924027294914a7c8bcebfae
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-04-14 11:20:22 -0300

    Update card.php

--- roupa/card.php
+++ roupa/card.php
@@ -20,6 +20,9 @@ $roupa->setTipo($_POST['tipo'])
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap" rel="stylesheet">
     <title>Document</title>
 
     <style>
@@ -42,6 +45,8 @@ $roupa->setTipo($_POST['tipo'])
             justify-content: center;
             display: flex;
             color: #613749;
+            font-family: "Imperial Script", cursive;
+            font-size: 80px;
             
         }
 
@@ -49,6 +54,7 @@ $roupa->setTipo($_POST['tipo'])
             justify-content: center;
             display: flex;
             color: #ffffff;
+        
         }
 
         .imagem {
